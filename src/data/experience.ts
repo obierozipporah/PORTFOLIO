4e80725b7bacafe8c35aec7f565d1a4239e32303
Zipporah Obiero <obierozipporah28@gmail.com>
2024-10-01 23:12:52 +0300

    ...
    ...

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -6,26 +6,26 @@ interface ExperienceInfoProps {
 export const experienceInfo: ExperienceInfoProps[] = [
   {
     language: 'HTML',
-    info: '3 Years Experience',
+    info: '2 Years Experience',
   },
   {
     language: 'CSS',
-    info: '3 Years Experience',
+    info: '2 Years Experience',
   },
   {
     language: 'Javascript',
-    info: '3 Years Experience',
+    info: '2 Years Experience',
   },
   {
     language: 'Node.js',
-    info: '3 Years Experience',
+    info: '2 Years Experience',
   },
   {
     language: 'React',
-    info: '3 Years Experience',
+    info: '2 Years Experience',
   },
   {
     language: 'Typescript',
-    info: '3 Years Experience',
+    info: '2 Years Experience',
   },
 ];
